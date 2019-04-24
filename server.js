32463face62aed8bddceb0fa51c58cb760c959b6
ehager77 <eric@isabel.io>
2019-04-23 23:51:44 -0700

    added event listeners

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // Node Dependencies
 var express = require('express');
+var path=require('path');
 // body-parser is depracated
 // var bodyParser = require('body-parser');
 
 var app = express();
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static(process.cwd() + '/public'));
 // app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Handlebars
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+// app.set('views',path.join(__dirname,'views'));
 
 
 var router = require('./controllers/burgers_controller.js');
