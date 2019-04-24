caa452e9aebc13ef4c1710ca0715c022a9b6bda2
ehager77 <eric@isabel.io>
2019-04-23 21:45:44 -0700

    set up orm, routes and db connection to Jaws

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+    
+-- Create the burgers_db database --
+USE l6qovsow9j04ro08;
+
+-- Create a burgers table with the required fields --
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+  	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	PRIMARY KEY(id)
+);
